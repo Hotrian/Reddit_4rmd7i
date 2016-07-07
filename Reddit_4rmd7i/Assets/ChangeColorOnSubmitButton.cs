5f03fe6debf7ec863c9db89930eba65121275aa5
Nicholas Ewalt <Hotrian@Gmail.com>
2016-07-06 21:46:14 -0700

    Init

---
+++ Reddit_4rmd7i/Assets/ChangeColorOnSubmitButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class ChangeColorOnSubmitButton : MonoBehaviour
+{
+    public MeshRenderer MeshRenderer
+    {
+        get { return _meshRenderer ?? (_meshRenderer = GetComponent<MeshRenderer>()); } // Obtain our MeshRenderer
+    }
+    private MeshRenderer _meshRenderer;
+
+	// Use this for initialization
+	void Start()
+	{
+	    EventController.Instance.OnClick += OnClick; // Listen for all "OnClick" Actions passed
+	    ToggleColor();
+	}
+
+    private void OnClick(string var)
+    {
+        if (var != "Submit") return; // Ignore "OnClick" Actions that are not "Submit"
+        ToggleColor();
+    }
+
+    private bool _isRed;
+
+    private void ToggleColor()
+    {
+        if (MeshRenderer != null)
+        {
+            if (_isRed)
+            {
+                MeshRenderer.material.color = Color.green;
+                _isRed = false;
+            }
+            else
+            {
+                MeshRenderer.material.color = Color.red;
+                _isRed = true;
+            }
+        }
+    }
+}
---
+++ Reddit_4rmd7i/Assets/EventController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class EventController : MonoBehaviour
+{
+    public static EventController Instance
+    {
+        get { return _instance ?? (_instance = FindObjectOfType<EventController>()); } // Return the instance or find it in the scene
+    }
+
+    private static EventController _instance;
+
+    public Action<string> OnClick;
+
+    public void DoClick(string var)
+    {
+        //Debug.Log("Do Click " + var);
+        if (OnClick != null)
+        {
+            OnClick(var);
+        }
+    }
+}
